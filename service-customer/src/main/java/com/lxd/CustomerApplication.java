ca9ff5eafb1b7b6dcc8b50e2c422a3f0b285929c
lxdd <896576159@qq.com>
2017-08-14 11:03:10 +0800

    modify project name

--- service-customer/src/main/java/com/lxd/CustomerApplication.java
+++ service-customer/src/main/java/com/lxd/CustomerApplication.java
@@ -3,12 +3,17 @@ package com.lxd;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ProducerApplication {
+@EnableFeignClients
+public class CustomerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProducerApplication.class, args);
+		SpringApplication.run(CustomerApplication.class, args);
 	}
+
+
+
 }
