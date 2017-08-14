27fffafb0ff1d6af99f8be0ad7bbbaf79de9596a
lxdd <896576159@qq.com>
2017-08-14 12:06:00 +0800

    web-lovehome  project commit

---
+++ web-lovehome/src/main/java/com/lxd/LovehomeApplication.java
@@ -0,0 +1,12 @@
+package com.lxd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LovehomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LovehomeApplication.class, args);
+	}
+}
